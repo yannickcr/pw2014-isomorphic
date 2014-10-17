16648e0df212e303effa5db202dadf1bc48abe12
Yannick Croissant <yannick.croissant@gmail.com>
2014-10-17 22:53:04 +0200

    Add submit handler on the form

--- History.md
+++ History.md
@@ -1,4 +1,8 @@
 
+0.0.4 / 2014-10-17
+==================
+ * Add submit handler on the form
+
 0.0.3 / 2014-10-17
 ==================
  * Add API endpoints
--- components/app.jsx
+++ components/app.jsx
@@ -1,16 +1,37 @@
 /** @jsx React.DOM */
 'use strict';
 
-var React = require('react');
+var React   = require('react');
+var request = require('superagent');
 
 var App = React.createClass({
+
+  addPost: function(e) {
+    e.preventDefault();
+
+    var content = this.refs.content.getDOMNode().value;
+
+    if (!content) {
+      return;
+    }
+
+    request
+      .post('http://localhost:8000/api/addPost')
+      .send({
+        content: content
+      })
+      .end();
+
+    this.refs.content.getDOMNode().value = '';
+
+  },
 
   render: function() {
     return (
       <section className="container">
         <h1>Isomorphic example</h1>
-        <form method="post" action="/api/addPost">
-          <div className="form-group"><textarea className="form-control" name="content"></textarea></div>
+        <form method="post" action="/api/addPost" onSubmit={this.addPost}>
+          <div className="form-group"><textarea className="form-control" name="content" ref="content"></textarea></div>
           <div className="form-group"><button className="btn btn-primary" type="submit">Submit</button></div>
         </form>
         <ul className="list-group">
